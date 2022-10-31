3d73212d26bff68316d390032359e597ce525003
Nika Drozdova <cherepawka13@mail.ru>
2022-10-31 14:07:05 +0300

    add task2

--- lib/src/main/java/com/drozdova/tms/lib/HomeWork4.java
+++ lib/src/main/java/com/drozdova/tms/lib/HomeWork4.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class HomeWork4 {
     public static void main(String[] args) {
-        task1();
+//        task1();
+        task2();
     }
 
     // Ввести n строк с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.
@@ -37,7 +38,43 @@ public class HomeWork4 {
             index++;
         }
 
-        System.out.println("Min len = " + array.get(minIndex).length() + ", " + array.get(minIndex));
+        System.out.println( "Min len = " + array.get(minIndex).length() + ", " + array.get(minIndex));
         System.out.println("Max len = " + array.get(maxIndex).length() + ", " + array.get(maxIndex));
+    }
+
+    // Ввести n строк с консоли. Вывести на консоль среднюю длину строк и те строки, длина которых больше средней.
+    // используйте StringBuilder для вывода всех строк, которые больше средней длины.
+    public static void task2() {
+        List<String> array = new ArrayList<>();
+        System.out.println("Введите количетво n строк...");
+        Scanner scan = new Scanner(System.in);
+        while (!scan.hasNextInt()) {
+            scan.next();
+            System.out.println("Ошибка ввода");
+        }
+
+        int n = scan.nextInt();
+        int averageLen = 0;
+        int index = 0;
+        String item;
+        while (index < n) {
+            item = scan.nextLine();
+            array.add(item);
+            averageLen += item.length();
+            index++;
+        }
+
+        averageLen /= n;
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            item = array.get(i);
+            if (item.length() > averageLen) {
+                builder.append(item);
+                builder.append("\n");
+            }
+        }
+
+        System.out.println(builder);
     }
 }
