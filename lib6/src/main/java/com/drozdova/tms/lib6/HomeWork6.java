6a1e839074ba92dccfa61f9c84e0d4517d5e85fd
Nika Drozdova <cherepawka13@mail.ru>
2022-10-31 16:17:22 +0300

    add task 1 class HomeWork6

--- lib6/src/main/java/com/drozdova/tms/lib6/HomeWork6.java
+++ lib6/src/main/java/com/drozdova/tms/lib6/HomeWork6.java
@@ -1,4 +1,17 @@
 package com.drozdova.tms.lib6;
 
 public class HomeWork6 {
+    public static void main(String[] args) {
+        CreditCard card1 = new CreditCard(12345687, 0);
+        CreditCard card2 = new CreditCard(52146258, 400);
+        CreditCard card3 = new CreditCard(65898426, 20);
+
+        card1.putMoney(125);
+        card2.putMoney(548);
+        card3.withdrawMoney(5);
+
+        System.out.println(card1);
+        System.out.println(card2);
+        System.out.println(card3);
+    }
 }
