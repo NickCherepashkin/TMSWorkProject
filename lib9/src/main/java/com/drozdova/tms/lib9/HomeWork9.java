1a1e3a0a7958e35ac3187e546fb5500e5b97fc67
Nika Drozdova <cherepawka13@mail.ru>
2022-11-09 15:08:13 +0300

    add task 1-4 multi arrays

--- lib9/src/main/java/com/drozdova/tms/lib9/HomeWork9.java
+++ lib9/src/main/java/com/drozdova/tms/lib9/HomeWork9.java
@@ -16,6 +16,10 @@ public class HomeWork9 {
         task5();
         task6();
         task7();
+        task_1_multiArray();
+        task_2_multiArray();
+        task_3_multiArray();
+        task_4_multiArray();
     }
 
     public static void task1() {
@@ -177,4 +181,100 @@ public class HomeWork9 {
 
         System.out.println(Arrays.toString(mas));
     }
-}   
+
+    public static void task_1_multiArray() {
+        int [][] mass = new int[3][3];
+        Random random = new Random();
+        for(int i = 0; i < mass.length; i++) {
+            for(int j = 0; j < mass[i].length; j++) {
+                mass[i][j] = random.nextInt(20);
+            }
+        }
+
+        for(int i = 0; i < mass.length; i++) {
+            System.out.println(Arrays.toString(mass[i]));
+        }
+    }
+
+    public static void task_2_multiArray() {
+        int [][][] mass = {{{1,3,2},{1,5,8,9},{1,1}},{{4,5,6,9,9},{3,4},{5,8,7},{0,0,2,3}},{{1,4,7,2,8},{3,3,9}}};
+        for(int i = 0; i < mass.length; i++) {
+            for(int j = 0; j < mass[i].length; j++) {
+                for(int k = 0; k < mass[i][j].length; k++) {
+                    System.out.println(mass[i][j][k]);
+                }
+            }
+        }
+
+        for(int i = 0; i < mass.length; i++) {
+            for(int j = 0; j < mass[i].length; j++) {
+                System.out.print(Arrays.toString(mass[i][j]));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void task_3_multiArray() {
+        int [][] mas = new int[2][6];
+        Random random = new Random();
+        for(int i = 0; i < mas.length; i++) {
+            for(int j = 0; j < mas[i].length; j++) {
+                mas[i][j] = random.nextInt(20);
+            }
+            System.out.println(Arrays.toString(mas[i]));
+        }
+
+        sortByAsc(mas[0]);
+        sortByDesc(mas[1]);
+        System.out.println("After sort:");
+        System.out.println(Arrays.toString(mas[0]));
+        System.out.println(Arrays.toString(mas[1]));
+    }
+
+    public static void sortByAsc(int [] mas) {
+        boolean isSorted = false;
+        int item;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    item = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = item;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    public static void sortByDesc(int [] mas) {
+        boolean isSorted = false;
+        int item;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] < mas[i + 1]) {
+                    item = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = item;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    public static void task_4_multiArray() {
+        Random random = new Random();
+        int len = random.nextInt(10);
+        int [][][] mass = new int[len][len][len];
+        for(int i = 0; i < mass.length; i++) {
+            for(int j = 0; j < mass[i].length; j++) {
+                for(int k = 0; k < mass[i][j].length; k++) {
+                    mass[i][j][k] = random.nextInt(10);
+                }
+                System.out.print(Arrays.toString(mass[i][j]) + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
